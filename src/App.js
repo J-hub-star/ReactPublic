bb7c50f04b3ea87217a9897b6cb02f42f81fc443
2hotGrantPurple <evilgamer610@gmail.com>
2021-06-01 15:19:04 -0500

    react-app-consumes a public api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import SearchBar from "../src/component/search-bar/search-bar.component.jsx";
+import CardList from "../src/component/card-list/CardList.jsx";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar></SearchBar>
+        <CardList></CardList>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
---
+++ src/component/card-list/CardList.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+const CardList = (props) => {return <div></div>}
+
+export default CardList;
